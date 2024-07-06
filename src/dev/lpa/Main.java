bed5a913e0fb26d0f70312012ecce427ec1c4ad3
Aspect9958 <waterjuice333@gmail.com>
2024-07-06 15:42:14 -0700

    Initial commit

---
+++ src/dev/lpa/Main.java
@@ -0,0 +1,10 @@
+package dev.lpa;
+
+public class Main {
+
+    // get shitted on niggerrrrr, shit on absolutely by the sigma king aarav ban.
+
+    public static void main(String[] args) {
+
+    }
+}
